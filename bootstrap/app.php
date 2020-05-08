ff8a4a4fceefcda3b1cb118fd95d8646db185081
ashkan fekri <ashkanfekridev@gmail.com>
2020-05-09 03:50:48 +0430

    connected web routes to bootstrap file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,12 +2,13 @@
     require __DIR__ . '/../vendor/autoload.php';
     require __DIR__ . '/../src/helper.php';
     require_once __DIR__ . '/../config/config.php';
+
     use App\Router;
     use App\Request;
+
     $router = new Router();
     $request = new Request();
 
-    $router->get('/', 'appController@index');
-
+    require_once __DIR__ . '/../app/http/routes/web.php';
 
     $router->runRouter($request->uri(), $request->method());
